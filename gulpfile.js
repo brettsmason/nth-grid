b467a4de32643c14acea86c673210dd3db3fa1e5
Brett Mason <brettsmason@gmail.com>
2017-10-06 10:03:10 +0100

    Huge testing update. Now includes grid mixin testing too!

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,36 @@
 var gulp    = require('gulp');
 var plugins = require('gulp-load-plugins');
 var browser = require('browser-sync');
+var rimraf = require('rimraf');
 
 var $ = plugins();
 
 // Build the SCSS into CSS
-gulp.task('build', gulp.parallel(copy, sass));
+gulp.task('build', gulp.series(clean, gulp.parallel(copy, sass)));
 
 // Build the SCSS, watch for changes.
 gulp.task('default',
 gulp.series('build', server, watch));
 
+// Clean the dist folder
+function clean(done) {
+  rimraf('dist', done);
+}
+
 // Copy demo index file
 function copy() {
-  return gulp.src(['demo/**/*.html'])
+  return gulp.src(['tests/visual/**/*.html'])
     .pipe(gulp.dest('dist'));
 }
 
 function sass() {
-  return gulp.src('demo/**/*.scss')
-    .pipe($.sourcemaps.init())
+  return gulp.src('tests/visual/**/*.scss')
     .pipe($.sass({
       includePaths: ['sass']
     }).on('error', $.sass.logError))
     .pipe($.autoprefixer({
       browsers: [ "last 2 versions", "ie >= 10", "ios >= 7" ]
     }))
-    .pipe($.sourcemaps.write())
     .pipe(gulp.dest('dist'))
     .pipe(browser.reload({ stream: true }));
   }
@@ -48,6 +52,6 @@ function reload(done) {
 }
 // Watch for changes to static assets, pages, Sass, and JavaScript
 function watch() {
-  gulp.watch(['sass/**/*.scss', 'demo/**/*.scss']).on('all', sass);
-  gulp.watch('demo/**/*.html').on('all', gulp.series(copy, browser.reload));
+  gulp.watch(['sass/**/*.scss', 'tests/visual/**/*.scss']).on('all', sass);
+  gulp.watch('tests/visual/**/*.html').on('all', gulp.series(copy, browser.reload));
 }
--- test/sass/test_sass.js
+++
@@ -1,8 +0,0 @@
-var path = require('path');
-var sassTrue = require('sass-true');
-
-// Test file
-var sassFile = path.join(__dirname, 'test.scss');
-
-// Run tests
-sassTrue.runSass({file: sassFile}, describe, it);
